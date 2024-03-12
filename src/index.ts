1d49df643740d5505e3fa7f07493e871b69c4b7b
Mathias Iden <mathias.iden@dnb.no>
2024-03-12 14:51:00 +0100

    Clean up some more

--- src/index.ts
+++ src/index.ts
@@ -5,29 +5,27 @@ import { ZodError, z } from "zod";
 const getNorwegianCurrentDate = () =>
   new Date(new Date().toLocaleString("en-US", { timeZone: "Europe/Oslo" }));
 
-const router = Router()
-  .get("/balance", (req) => {
-    const { payDayOfMonth, salary } = z
-      .object({
-        payDayOfMonth: z.number({ coerce: true }).gte(1).lte(28),
-        salary: z.number({ coerce: true }).positive().safe(),
-      })
-      .parse(req.query);
+const BalanceQueries = z.object({
+  payDayOfMonth: z.number({ coerce: true }).gte(1).lte(28),
+  salary: z.number({ coerce: true }).positive().safe(),
+});
 
-    return {
-      ...getSuggestedEndOfDayBalance(
-        payDayOfMonth,
-        salary,
-        getNorwegianCurrentDate()
-      ),
-      noTime: getNorwegianCurrentDate(),
-    };
-  })
-  .all("*", () => error(404));
+const router = Router();
+
+router.get("/balance", (req) => {
+  const { payDayOfMonth, salary } = BalanceQueries.parse(req.query);
+  const norwegianCurrentDate = getNorwegianCurrentDate();
+
+  return {
+    ...getSuggestedEndOfDayBalance(payDayOfMonth, salary, norwegianCurrentDate),
+    noTime: norwegianCurrentDate,
+  };
+});
 
 export default {
   fetch: (request: Request, ...args: any) =>
     router
+      .all("*", () => error(404))
       .handle(request, ...args)
       .then(json)
       .catch((error) => {
