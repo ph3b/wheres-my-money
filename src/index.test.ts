b3f9a5e8ee923553100244798f8e7d04e892638a
Mathias Iden <mathias.iden@dnb.no>
2024-04-05 22:21:28 +0200

    Fix test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,12 @@
-import { test, expect } from "vitest"
+import { test, expect } from "vitest";
 import { SELF as worker } from "cloudflare:test";
-import "./index"
+import "./index";
 
 test("Run integration smoke test", async () => {
-  const response = await worker.fetch("http://example.com/balance?salary=10000&payDayOfMonth=10");
-  const body = await response.json() as any;
+  const response = await worker.fetch(
+    "http://example.com/wmm/balance?salary=10000&payDayOfMonth=10"
+  );
+  const body = (await response.json()) as any;
 
   expect(response.status).toBe(200);
   expect(body.balance).toBeDefined();
